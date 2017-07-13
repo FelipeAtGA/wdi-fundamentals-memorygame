475eadde02ea76095801ff95632ce26ffb957daf
Felipe Hernandez <felipeneo@hotmail.com>
2017-07-13 01:07:05 -0400

    add object array to js code

--- js/main.js
+++ js/main.js
@@ -1,6 +1,29 @@
 console.log("Up and running!");
 
-var cards = ['queen', 'queen', 'king', 'king'];
+var cards = [
+	{
+		rank: 'queen',
+		suit: 'hearts',
+		cardImage: 'images/queen-of-hearts.png'
+
+	},
+	{
+		rank: 'queen',
+		suit: 'diamonds',
+		cardImage: 'images/queen-of-diamonds.png'		
+	},
+	{
+		rank: 'king',
+		suit: 'hearts',
+		cardImage: 'images/king-of-hearts.png'
+	},
+	{
+		rank: 'king',
+		suit: 'diamonds',
+		cardImage: 'images/king-of-hearts.png'
+	}
+
+];
 
 var cardsInPlay = [];
 
@@ -10,22 +33,18 @@ var checkForMatch = function(){
   	} else {
       	console.log("Sorry, try again.");
   	}
-}
+
+};
 
 var flipCard = function(cardId){
-	console.log("User flipped " + cards[cardId] );
-	cardsInPlay.push(cards[cardId]);
-	if (cardsInPlay.length === 2){
-		if (cardsInPlay[0] === cardsInPlay[1]){
-			alert("You found a match!");
-		}else{
-			alert("Sorry, try again");
-		}
-	}
+	console.log("User flipped " + cards[cardId].rank );
+	cardsInPlay.push(cards[cardId].name);
+  	console.log(cards[cardId].cardImage);
+  	console.log(cards[cardId].suit);
 	checkForMatch();
 
-}
+};
 
 flipCard(0);
-flipCard(2);
+flipCard(1);
 
