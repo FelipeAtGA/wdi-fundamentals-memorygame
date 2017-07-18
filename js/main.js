5dc0ea72513fe5087b8607023aa73e8a9e281f20
Felipe Hernandez <felipeneo@hotmail.com>
2017-07-18 08:54:00 -0400

    Fix logic and add new message

--- js/main.js
+++ js/main.js
@@ -29,11 +29,18 @@ var cards = [
 var cardsInPlay = [];
 
 var checkForMatch = function(){
-	
+	console.log(cardsInPlay.sort());
 	 if (cardsInPlay[0] === cardsInPlay[1]) {
-      	alert("You found a match!");
+	 	var displMsg = document.getElementById('msg');
+	 	//displMsg.setAttribute('class', 'visible');
+	 	displMsg.innerHTML =  '<h1>WINNER</h1>';
+      	//alert("You found a match!");
   	} else {
-      	alert("Sorry, try again.");
+	 	var displMsg = document.getElementById('msg');
+	 	//displMsg.setAttribute('class', 'visible');
+	 	displMsg.innerHTML =  '<h4>Sorry try again</h4>';
+ 		
+      	//alert("Sorry, try again.");
   	}
 
 };
@@ -41,15 +48,25 @@ var checkForMatch = function(){
 var flipCard = function(){
 	var cardId = this.getAttribute('data-id');
 	this.setAttribute('src', cards[cardId].cardImage);
-	console.log("User flipped " + cards[cardId].rank);
-	cardsInPlay.push(cards[cardId].name);
-  	console.log(cards[cardId].cardImage);
-  	console.log(cards[cardId].suit);
-	checkForMatch();
+	//console.log("User flipped " + cards[cardId].rank);
+	cardsInPlay.push(cards[cardId].rank);
+  	//console.log(cards[cardId].cardImage);
+  	//console.log(cards[cardId].suit);
+  	if(cardsInPlay.length === 2){
+  		checkForMatch();
+  	}else{
+  		flipCard();
+  	}
 
 };
 
 var createBoard = function(){
+	var message = document.createElement('h1');
+	message.setAttribute('id', 'msg');
+	//message.setAttribute('class', 'hidden');
+	//message.textContent = 'WINNER';
+	document.getElementById('game-board').appendChild(message);
+
 	for(var i = 0; i < cards.length; i += 1){
 		var cardElement = document.createElement('img');
 		cardElement.setAttribute('src', 'images/back.png');
